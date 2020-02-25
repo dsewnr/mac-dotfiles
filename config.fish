4dafa023e246ad0167a77e21f53f4953e27a10e8
peter-br-lu <peter_br_lu@trendmicro.com>
2020-02-25 13:28:24 +0800

    Enable GPG and vi mode

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
 set -U autovenv_enable yes
 set -gx LDFLAGS "-L/usr/local/opt/libffi/lib"
+set -x GPG_TTY (tty)
 
 alias vim="nvim"
 alias venv="source venv/bin/activate.fish"
@@ -8,3 +9,4 @@ alias venv="source venv/bin/activate.fish"
 
 fish_ssh_agent
 set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
+fish_vi_key_bindings
