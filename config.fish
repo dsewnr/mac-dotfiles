2b3ffc5fe2e9285f424a9cd728a37657a96e1f0c
Peter BR Lu <peter_br_lu@trendmicro.com>
2020-12-14 11:26:13 +0800

    Update config

--- config.fish
+++ config.fish
@@ -2,9 +2,11 @@ set -U autovenv_enable yes
 set -gx LDFLAGS "-L/usr/local/opt/libffi/lib"
 set -x GPG_TTY (tty)
 set -x EDITOR vim
+set -x FZF_DEFAULT_OPTS "--height 40% --layout=reverse --border"
 
 alias vi="vim"
 alias vim="nvim"
+alias vim_fzf="vim (fzf)"
 alias venv="source venv/bin/activate.fish"
 alias gpg_restart="killall gpg-agent && gpg-agent --daemon --use-standard-socket --pinentry-program /usr/local/bin/pinentry-curses"
 
